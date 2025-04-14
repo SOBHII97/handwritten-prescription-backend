02b84f9da85646972988ab87e03fa562539deb0a
Adelrahman Sobhi <sobhii7420@gmail.com>
2025-04-14 03:14:25 +0200

    auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
-from app.config import mongo  # متنساش تربطه في config.py
+from app.config import mongo  
 
 auth_bp = Blueprint('auth', __name__)
 
